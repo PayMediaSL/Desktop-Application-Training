7056a7a9faeb6afeb2ccfd7cdc28496ccb74b1c7
Isuru Lakmina <58630360+lakmina456@users.noreply.github.com>
2025-02-19 13:08:53 +0530

    updates

---
+++ LearningDesctopApplication/Actions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace LearningDesctopApplication
+{
+    public static class Actions
+    {
+
+        public static string HashPassword(string password)
+        {
+            SHA256 sha256 = SHA256.Create();
+            byte[] inputBytes = Encoding.UTF8.GetBytes(password);
+            byte[] hashBytes = sha256.ComputeHash(inputBytes);
+            return Convert.ToHexString(hashBytes);
+        }
+
+        public static void ValidateNIC(string nic, TextCompositionEventArgs e)
+        {
+
+            Regex regex = new(@"^\d{0,8}$|^\d{9}[Vv]?$|^\d{0,12}$");
+            string nextInput = nic + e.Text;
+            if (!regex.IsMatch(nextInput))
+            {
+                e.Handled = true;
+            }
+        }
+
+
+        public static bool ValidatePassword(string password)
+        {
+            Regex regex = new(@"^(?=.*[A-Z])(?=.*\W).{8,}$");
+            return regex.IsMatch(password);
+        }
+        public static bool IsValidEmail(string email)
+        {
+            string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, emailPattern);
+        }
+    }
+    
+}
---
+++ LearningDesctopApplication/Model/DbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace LearningDesctopApplication.Model
+{
+    public class DbContext
+    {
+        [Key]
+        public string NIC { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+    public class ApplicationContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbSet<DbContext> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;database=AuthSystem;user=root;password=admin",
+                new MySqlServerVersion(new Version(8, 0, 21)));
+        }
+    }
+}
